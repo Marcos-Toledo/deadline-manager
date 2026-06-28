659b93b64b233eb7349f65f3df520779097cb079
Marcos-Toledo <xquinhox@gmail.com>
2026-06-28 16:24:52 -0300

    resolving build error

--- app/(public)/(auth)/layout.tsx
+++ app/(public)/(auth)/layout.tsx
@@ -1,14 +1,18 @@
+import { Suspense } from "react";
+
 export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen w-full flex items-stretch">
-      <div className="bg-[url('/img/bg-auth.png')] bg-cover bg-center bg-no-repeat flex-2 hidden lg:block"></div>
-      <div className="flex-1 flex items-center justify-center shadow-2xl px-4 py-8 relative">
-        {children}
+    <Suspense>
+      <div className="min-h-screen w-full flex items-stretch">
+        <div className="bg-[url('/img/bg-auth.png')] bg-cover bg-center bg-no-repeat flex-2 hidden lg:block"></div>
+        <div className="flex-1 flex items-center justify-center shadow-2xl px-4 py-8 relative">
+          {children}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
