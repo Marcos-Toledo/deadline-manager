3feffb4665165cfff526a7f0ffd1dc2bd626e626
Marcos-Toledo <xquinhox@gmail.com>
2026-07-01 16:35:31 -0300

    bg image auth

--- app/(public)/(auth)/layout.tsx
+++ app/(public)/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Suspense } from "react";
 
 export default async function AuthLayout({
@@ -8,7 +9,15 @@ export default async function AuthLayout({
   return (
     <Suspense>
       <div className="min-h-screen w-full flex items-stretch">
-        <div className="bg-[url('/img/bg-auth.png')] bg-cover bg-center bg-no-repeat flex-2 hidden lg:block"></div>
+        <div className="flex-2 hidden lg:block relative">
+          <Image
+            src="/img/bg-auth.png"
+            alt="Background"
+            width={1920}
+            height={1080}
+            className="w-full h-full object-cover"
+          />
+        </div>
         <div className="flex-1 flex items-center justify-center shadow-2xl px-4 py-8 relative">
           {children}
         </div>
